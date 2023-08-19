1919ad541a8f19d3c196f51c004c9348a87ee9ff
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-08-19 23:12:14 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ const express = require('express');
 require('dotenv').config();
 const db = require('./db');
 const userRoutes = require('./routes/user.routes')
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
 const Port = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(cors());
+
 
 app.use(
     bodyParser.urlencoded({
